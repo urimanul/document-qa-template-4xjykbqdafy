f0135562aa3102fda0214abddbdf864b5f4a3032
urimanul <myopera9@gmail.com>
2024-07-09 12:14:04 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,6 +33,7 @@ cur.close()
 
 # Show title and description.
 st.title("📄 Document question answering")
+st.write(openai_api_key)
 st.write(
     "Upload a document below and ask a question about it – GPT will answer! "
     "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
